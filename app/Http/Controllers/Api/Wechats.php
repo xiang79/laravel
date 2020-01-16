36c18f36c70747ccb6c43569e9bf9645a23a5337
xiang <2127227052@qq.com>
2020-01-16 19:51:06 +0800

    99

--- app/Http/Controllers/Api/Wechats.php
+++ app/Http/Controllers/Api/Wechats.php
@@ -9,20 +9,18 @@ use App\Tools\Wechat;
 class Wechats extends Controller
 {
       public function index(Request $request){
-//            echo 1111222221;exit;
 //            $echostr=$request->input("echostr");
 //            echo $echostr;die;
             $xmlStr =file_get_contents("php://input");
-            // var_dump($xmlStr);
           // file_put_contents("1.txt",$xmlStr);
-            //把xml数据转化成对象
-            $xmlObj=simplexml_load_string($xmlStr,'SimpleXMLElement',LIBXML_NOCDATA);
-            // var_dump($xmlObj);
+            //把xml数据转化成对象,'SimpleXMLElement',LIBXML_NOCDATA
+            $xmlObj=simplexml_load_string($xmlStr);
+             var_dump($xmlObj);
             #用户扫码关注事件
             if($xmlObj->MsgType == 'event' && $xmlObj->Event == 'subscribe'){
                   //(string)  强制转化为字符串
-                  $FromUserName=$xmlObj->FromUserName;
-                  $EventKey=$xmlObj->EventKey;#获取标识
+                  $FromUserName=(string)$xmlObj->FromUserName;
+                  $EventKey=(string)$xmlObj->EventKey;#获取标识
                   //ltrim  截取左边的字符
                   $event=ltrim($EventKey,'qrscene_');
             if($event){
