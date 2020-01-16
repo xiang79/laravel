c7940f4fd06a66a4224d129ac656a271e919a3ae
xiang <2127227052@qq.com>
2020-01-16 11:23:05 +0800

    tdssjsss

--- app/Http/Controllers/Api/Wechats.php
+++ app/Http/Controllers/Api/Wechats.php
@@ -9,7 +9,7 @@ use App\Tools\Wechat;
 class Wechats extends Controller
 {
       public function index(Request $request){
-            // echo 11111;exit;
+            echo 11111;exit;
             $echostr=$request->input("echostr");
             echo $echostr;die;
             $xmlStr =file_get_contents("php://input");
